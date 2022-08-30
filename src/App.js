9a014c4a7437ad1a96d1dc4cc09d8dae2c9ae76d
Adrián Emanuel Koenig <adrian.95.koenig@gmail.com>
2022-08-30 18:53:25 -0300

    ItemCount

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { useState } from "react";
 import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/NavBar';
+import ItemCount from './components/ItemCount';
 
 function App() {
 
-  return (
+const [initial, setInitial] = useState(1);
+const [stock, setStock] = useState(5);
+const onAdd = () => {initial < stock && setInitial(initial + 1)};
+const Remove = () => {initial > 1 && setInitial(initial - 1)};
+
+return (
     <div className="App">
       <body>
       <header >
@@ -12,6 +19,7 @@ function App() {
       </header>
       <main>
           <ItemListContainer greeting={"Gibson"} />
+          <ItemCount stock={stock} initial={initial} onAdd={onAdd} Remove={Remove} />
       </main>
       </body>
     </div>
