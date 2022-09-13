6972f8d2713a0bd9ce0a5a13f73f9a1ea8e49751
Adrián Emanuel Koenig <adrian.95.koenig@gmail.com>
2022-09-13 18:56:59 -0300

    eventos

--- src/components/Item Detail/ItemDetail.jsx
+++ src/components/Item Detail/ItemDetail.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ItemCount from '../ItemCount'
 import "./itemDetail.css";
 
 function ItemDetail({productDetail}) {
-	const {mark, tag, img, price, stock} = productDetail;
-    const onAdd = () =>{
-        console.log("Bought");
+    const[count, setCount]=useState(1)
+    const [bought, setBought] = useState(false)
+    const {mark, tag, img, price, stock} = productDetail;
+
+    const onAdd = () => {
+        console.log(`Bought: ${count} ${mark} ${tag}`);
+        setBought(true)
     }
 
     return (
         <div className='Item-Detail'>
             <h3>{mark}</h3>
             <h4>{tag}</h4>
-            <img src={img}/>
+            <img src={img} alt={tag}/>
             <p>${price}</p>
             <p>Stock: {stock}</p>
-            <ItemCount stock={stock} initial={1} onAdd={onAdd}/>
+            {!bought
+                ? <ItemCount stock={stock} initial={1} onAdd={onAdd} count={count} setCount={setCount}/>
+                : <button className='btnCart'>Go to cart</button>
+            }
         </div>
     )
 }
